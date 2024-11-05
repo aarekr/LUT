889576c641d08e8316706dd2ea21ba803bb6e73b
aarekr <aare212@hotmail.com>
2024-11-05 10:05:05 +0200

    2.3. Extending interface

--- AdvWebApps/Week-2/dist/app.js
+++ AdvWebApps/Week-2/dist/app.js
@@ -7,3 +7,29 @@ let vehicle = {
     power: 60
 };
 console.log(vehicle);
+;
+let car = {
+    model: "Ford focus",
+    color: "Green",
+    year: 2016,
+    power: 150,
+    bodyType: "Hatchback",
+    wheelCount: 4
+};
+let plane = {
+    model: "Boeing 777",
+    color: "White",
+    year: 2020,
+    power: 170000,
+    wingSpan: 65
+};
+let boat = {
+    model: "Bella",
+    color: "Black",
+    year: 2022,
+    power: 100,
+    draft: 0.42
+};
+console.log(car);
+console.log(plane);
+console.log(boat);
