988485995cdc381cfadd5c3bc2fb7055765b1e87
aarekr <aare212@hotmail.com>
2024-11-17 09:18:16 +0200

    3.2. ID echoing

--- AdvWebApps/Week-3/dist/src/index.js
+++ AdvWebApps/Week-3/dist/src/index.js
@@ -4,7 +4,7 @@ const express_1 = require("express");
 const router = (0, express_1.Router)();
 let poems = [];
 router.get("/", (req, res) => {
-    console.log("juurihakemisto");
+    //console.log("juurihakemisto")
     res.json(poems);
 });
 router.get("/hello", (req, res) => {
@@ -12,5 +12,10 @@ router.get("/hello", (req, res) => {
         msg: "Hello world!"
     };
     res.json(item);
+});
+router.get("/echo/:id", (req, res) => {
+    let id = req.params.id;
+    let answer = `id: ${id}`;
+    res.json(answer);
 });
 exports.default = router;
--- AdvWebApps/Week-3/src/index.ts
+++ AdvWebApps/Week-3/src/index.ts
@@ -5,7 +5,7 @@ const router: Router = Router()
 let poems: string[] = []
 
 router.get("/", (req: Request, res: Response) => {
-    console.log("juurihakemisto")
+    //console.log("juurihakemisto")
     res.json(poems)
 })
 
@@ -14,6 +14,12 @@ router.get("/hello", (req: Request, res: Response) => {
         msg: "Hello world!"
     }
     res.json(item)
+})
+
+router.get("/echo/:id", (req: Request, res: Response) => {
+    let id: string = req.params.id
+    let answer = `id: ${id}`
+    res.json(answer)
 })
 
 export default router
