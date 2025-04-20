61546884b20ed6a6c444d63284b8f91b27392a08
aarekr <aare212@hotmail.com>
2025-04-20 10:21:18 +0300

    Deleted old files

--- AdvWebApps/Week-4/dist/data/users.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const populateUsers = [
-    { "user": "Matti" },
-    { "user": "Maija" }
-];
-exports.default = populateUsers;
