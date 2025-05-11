db7bc82b90cced385454f8bf4572ecfb788d6fbf
aarekr <aare212@hotmail.com>
2025-05-11 19:24:53 +0300

    Middlewares and html files with servers

--- Node-Crash-Course/server.js
+++ Node-Crash-Course/server.js
@@ -1,20 +1,30 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 
-const PORT = process.env.PORT; //8000;
+const PORT = 8080;// process.env.PORT;
 
-const server = http.createServer((req, res) => {
+// Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname((__filename));
+
+const server = http.createServer(async (req, res) => {
     try {
         if (req.method === 'GET') {
+            let filePath;
             if (req.url === '/') {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end('<h1>Helou World!</h1>');
+                filePath = path.join(__dirname, 'public', 'index.html');
             } else if (req.url === '/about') {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end('<h1>About</h1>');
+                filePath = path.join(__dirname, 'public', 'about.html');
             } else {
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                res.end('<h1>Not Found</h1>');
+                throw new Error('not Found');
             }
+
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
         } else {
             throw new Error('Method Not Allowed');
         }
