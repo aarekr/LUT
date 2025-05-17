bc92f3f95d96627674d37f94615f3dd59aa55e51
aarekr <aare212@hotmail.com>
2025-05-17 12:54:32 +0300

    watch flag, sendFile, static web server

--- Express-Crash-Course/server.js
+++ Express-Crash-Course/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>  plain text');
+// setup static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*app.get('/', (req, res) => {
+    //res.send('<h1>Hello World and Watch</h1>  plain text');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.get('/about', (req, res) => {
+    //res.send('<h1>About</h1>');
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});*/
 
 app.listen(8000, () => console.log(`Server is running on port 8000`));
