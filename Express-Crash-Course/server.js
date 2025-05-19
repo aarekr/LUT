7ff816045381f9286bd4a5c6ebcaa93ad984b23b
aarekr <aare212@hotmail.com>
2025-05-19 12:36:45 +0300

    Router, post and put requests

--- Express-Crash-Course/server.js
+++ Express-Crash-Course/server.js
@@ -1,47 +1,18 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
 const port = process.env.PORT || 8000;
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false} ));
+
 // setup static folder
 //app.use(express.static(path.join(__dirname, 'public')));
 
-let posts = [
-    {id: 1, title: 'Post One'},
-    {id: 2, title: 'Post Two'},
-    {id: 3, title: 'Post Three'},
-];
-
-// Get all posts
-app.get('/api/posts', (req,res) => {
-    const limit = parseInt(req.query.limit);
-    if (!isNaN(limit) && limit > 0) {
-        res.status.json(posts.slice(0, limit));
-    } else {
-        res.status(200).json(posts);
-    }
-});
-
-// Get single post
-app.get('/api/posts/:id', (req,res) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id);
-    if (!post) {
-        res.status(404).json({ msg: `A post with the id of ${id} was not found` });
-    } else {
-        res.status(200).json(post);
-    }
-});
-
-/*app.get('/', (req, res) => {
-    //res.send('<h1>Hello World and Watch</h1>  plain text');
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/about', (req, res) => {
-    //res.send('<h1>About</h1>');
-    res.sendFile(path.join(__dirname, 'public', 'about.html'));
-});*/
+// Routes
+app.use('/api/posts', posts);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
