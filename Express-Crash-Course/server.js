c0f7bf22a56b24084ac41da96e088cf5aca4f020
aarekr <aare212@hotmail.com>
2025-05-23 21:02:36 +0300

    Dirname workaround

--- Express-Crash-Course/server.js
+++ Express-Crash-Course/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import posts from './routes/posts.js';
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/error.js';
@@ -7,15 +8,21 @@ import notFound from './middleware/notFound.js';
 
 const port = process.env.PORT || 8000;
 
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false} ));
+
+// Logger middleware
 app.use(logger);
 
 // setup static folder
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api/posts', posts);
