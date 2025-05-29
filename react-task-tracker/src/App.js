7d558ccdc52d755647d86255fc40fddbfe154cc3
aarekr <aare212@hotmail.com>
2025-05-29 12:55:05 +0300

    Handling events, useState, global state

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Eka',
+        day: 'Ma',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Toka',
+        day: 'Ti',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Kolmas',
+        day: 'KE',
+        reminder: false,
+    },
+  ])
   return (
     <div className="container">
       <Header title='Hello' />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -2,11 +2,14 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('click')
+  }
   return (
     <header className='header'>
       <h1 style={{ color: 'red', backgroundColor: 'black' }}>{title} Task Tracker</h1>
       <h2 style={headingStyle}>H2</h2>
-      <Button color='green' text='Add' />
+      <Button color='green' text='Add' onClick={onClick} />
     </header>
   )
 }
