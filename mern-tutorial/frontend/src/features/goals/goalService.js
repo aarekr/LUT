bfb55266ca95f7383cf6a1b2aaf2f7722782db67
aarekr <aare212@hotmail.com>
2025-06-16 18:19:15 +0300

    Fetching goals from server, async thunk, extra reducer

--- mern-tutorial/frontend/src/features/goals/goalService.js
+++ mern-tutorial/frontend/src/features/goals/goalService.js
@@ -1,20 +1,32 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
 
-const initialState = {
-    goals: [],
-    isError: false,
-    isSuccess: false,
-    isLoading: false,
-    message: ''
+const API_URL = '/api/goals/'
+
+// Create new goal
+const createGoal = async (goalData, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+    const response = await axios.post(API_URL, goalData, config)
+    return response.data
 }
 
-export const goalSlice = createSlice({
-    name: 'goal',
-    initialState,
-    reducers: {
-        reset: (state) => initialState
+// Get user goals
+const getGoals = async (token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
     }
-})
+    const response = await axios.get(API_URL, config)
+    return response.data
+}
 
-export const { reset } = goalSlice.actions
-export default goalSlice.reducer
+const goalService = {
+    createGoal,
+    getGoals,
+}
+
+export default goalService
